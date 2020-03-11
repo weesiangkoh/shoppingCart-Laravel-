6b9af792107db9f5f8485e0ce9e8279ba9be5cdd
ycbeng <yangcheebeng@hotmail.com>
2020-03-11 10:43:53 +0800

    modify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel <img src="{{ asset('image/logo.png') }}" alt="">
+                    My Cart <img src="{{ asset('image/logo.png') }}" alt="">
                 </div>
 
                 <div class="links">
